7fc3f6030bd0e5abf6dd4c0670c30259d0772528
LaJanae Davis <ldavis@LaJanaes-MacBook-Pro.local>
2024-07-04 14:28:17 -0400

    Components Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Home from "./components/Home";
-import Header from "./shared/Header";
-import Footer from "./shared/Footer";
+// import Header from "./shared/Header";
+// import Footer from "./shared/Footer";
 import About from "./components/About";
 import Admin from "./components/Admin";
 import Create from "./components/Create";
@@ -14,13 +14,14 @@ function App() {
   return (
     <div className="App">
       <Home/>
-      <Header />
-      <Footer />
+      {/* <Header /> */}
       <About />
       <Admin/>
       <Create/>
       <Login/>
       <Update/>
+      {/* <Footer /> */}
+
     </div>
   );
 }
